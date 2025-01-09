780ff093ccec3c654fad9e22f590f4685caa4f1f
sarkb02 <sarkoziboroka@gmail.com>
2025-01-09 09:53:48 +0100

    Aufgaben 0 und 1 Lösungen

---
+++ main.py
@@ -0,0 +1,15 @@
+#todo Aufgabe 0
+#Erstelle eine Variable
+
+n_loop = 0
+
+#todo Aufgabe 1
+#Gib alle Zahlen von 1 bis 100 aus
+n = 1
+while (n<=100):
+    print(n)
+    n = n+1
+
+#todo Aufgabe 2
+#ERsetze alle Zahlen, die durch 3 teilbar sind, durch "digital"
+
