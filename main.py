8014e082406d96889dc665059650dc628fb0b274
sarkb02 <sarkoziboroka@gmail.com>
2025-01-09 10:08:03 +0100

    while SChleife - durch 5 teilbar

--- main.py
+++ main.py
@@ -11,5 +11,22 @@ while (n<=100):
     n = n+1
 
 #todo Aufgabe 2
-#ERsetze alle Zahlen, die durch 3 teilbar sind, durch "digital"
+#Ersetze alle Zahlen, die durch 3 teilbar sind, durch "digital"
 
+n = 1
+while (n<=100):
+    if(n%3==0):
+        print("digital")
+    else: print(n)
+    n = n+1
+
+#todo Aufgabe 3
+# Erstetze alle Zahlen die durch 5 teilbar sind durch "history"
+
+n = 1
+while (n <= 100):
+    if (n % 5 == 0):
+        print("history")
+    else:
+        print(n)
+    n = n + 1
