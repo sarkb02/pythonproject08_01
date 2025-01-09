03fa3bc98853cfa5a241089aeb0b13968b66ebd4
sarkb02 <sarkoziboroka@gmail.com>
2025-01-09 10:14:34 +0100

    while SChleife - durch 3/5/beides teilbar

--- main.py
+++ main.py
@@ -30,3 +30,18 @@ while (n <= 100):
     else:
         print(n)
     n = n + 1
+
+#Ersetze alle Zahlen die durch 5 teilbar sind durch "history", die durch 3 teilbar sind durch "digital" und die durch beide durch "digital history"
+
+n = 0
+
+while n<=100:
+    n += 1
+    if (n%15==0): #oder: if x and y
+        print("digital history")
+    elif (n%5==0):
+        print("history")
+    elif (n%3==0):
+        print("digital history")
+    else:
+        print(n)
